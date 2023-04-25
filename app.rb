1f2a3476dc44c9d6463e3f14a05a164aaea59b6d
DanisMusicNTI <danis.music@elev.ga.ntig.se>
2023-04-25 20:07:03 +0200

    yardock KLAAAR

--- app.rb
+++ app.rb
@@ -6,16 +6,43 @@ require 'sqlite3'
 require 'bcrypt'
 require_relative 'model'
 
+
 enable :sessions
 
-db = SQLite3::Database.new("db/characters.db")
-db.results_as_hash = true
+
+# This method checks if the user is authorized to access the requested and redirects if they arent
+# 
+# @param [Array] restricted_paths An array with the names of the restriced routes
+# @param [Integer] user_id The id of the user
+# @return [void]
+before do
+  restricted_paths = ['/characters/','/characters/*', '/adminSite/','/adminSite/*', '/mainsite', '/shareChar/', '/shareChar/*']
+  user_id = session[:id]
+  @permission = DbAccesor.new.permissions(user_id)
+
+  if session[:id] == nil && restricted_paths.include?(request.path_info)   
+    redirect '/'
+  end
+ 
+  if session[:logged_in] && @permission["permissions"] != 1 && request.path_info == '/adminSite'
+    redirect '/'
+  end
+end
 
 
-get '/register' do
+# Display the register form
+# 
+# @return [String] the render of the registration form
+get '/register/' do
   slim :register
 end
 
+# Registers a new user with the provided username and password
+# 
+# @option [String] :username the username of the user
+# @option [String] :password the password of the user
+# @option [String] :password_confirm the password of the user repeated
+# @return [void]
 post '/users/new' do
   username = params[:username] 
   password = params[:password]
@@ -24,59 +51,90 @@ post '/users/new' do
   if (password == password_confirm)
     encoded_password = BCrypt::Password.create(password)
     db.execute("INSERT INTO users (username, password) VALUES (?, ?)", username, encoded_password)
-    redirect("/login")
+    redirect("/login/")
   else
     p "Lösenorden matchar inte, försök igen!"
-    redirect("/register")
+    redirect("/register/")
   end
 end
 
-get '/login' do
+# Display the login form
+# 
+# @return [String] the render of the login form
+get '/login/' do
   slim :login
 end
 
-get "/index" do
-  redirect("/") unless session[:id]
+# Displays the main site
+# 
+# @return [void]
+get "/mainsite" do
   user_id = session[:id]
-  @permission = db.execute("SELECT permissions FROM users WHERE id = ?", user_id).first
-  p @permission
-  @username = db.execute('SELECT username FROM users WHERE id = ?', session[:id]).first
-  slim :index
+  @permission = DbAccesor.new.permissions(user_id)
+  @username = DbAccesor.new.username(user_id)
+  slim :mainsite
 end
 
+# Authenticates a user with username and password and updates their session if succesfull
+# 
+# @option [String] :username the username of the user
+# @option [String] :password the password of the user
+# @return [void]
 post "/login/update" do
+  if session[:time] == nil
+    session[:time] = Time.new()
+  elsif Time.new() - session[:time] < 10
+    redirect to("/mainsite")
+  end
   @username = params[:username]
   password = params[:password]
-  db.results_as_hash = true
-  @result = db.execute('SELECT * FROM users WHERE username = ?', @username).first
-  check_password = @result["password"].to_s
+  @result = DbAccesor.new.specific_user(@username)
+  p @result
+  check_password = @result["password"]
   user_id = @result["id"]
-
-  p check_password
-  if (BCrypt::Password.new(check_password) == password)
-    session[:id] = user_id
-    session[:username] = db.execute('SELECT username FROM users WHERE id = ?', session[:id])
-   
-    redirect("/index")
-  else
-    "FEL LÖSENORD"
+  
+  username, user_id = DbAccesor.new.passwordCheck(password, check_password, user_id)
+  if username == nil
+    session[:time] = Time.new()
+    redirect to("/")
   end
+  session[:id] = user_id 
+  session[:username] = username
 
+  redirect to("/mainsite")
 end
 
-
+# Displays the home page
+# 
+# @return [void]
 get '/' do
   slim :homePage
 end
 
-get '/new' do
-  redirect("/") unless session[:id]
+# Displays the create characters site
+# 
+# @return [void]
+get '/characters/new' do
+  # redirect("/") unless session[:id]
   slim (:"characters/new")
 end
 
-
-post '/characters/new' do
-  # Create a new character in the database
+# Create a new character in the database
+# 
+# @option [String] :name The name of the character
+# @option [String] :race The race of the character
+# @option [String] :class The characters class
+# @option [Integer] :level The characters level
+# @option [Integer] :str The strength of the character
+# @option [Integer] :dex The dexterity of the character
+# @option [Integer] :con The constitution of the character
+# @option [Integer] :int The intelligence of the character
+# @option [Integer] :wis The wisdom of the character
+# @option [Integer] :cha The charisma of the character
+# @option [Integer] :user_id The id of the user creating the character
+# 
+# @return [void]
+post '/characters' do
   DbAccesor.new.create_character(
     name = params[:name],
     race = params[:race],
@@ -90,68 +148,93 @@ post '/characters/new' do
     cha = params[:cha],
     user_id = session[:id]
   )
-  redirect to('/index')
+  redirect to('/mainsite')
 end
 
-get '/characters' do
-  # Get all characters from the database
+# Get all characters associated with the current user or get all characters if user is an admin
+# 
+# @return [void]
+get '/characters/' do
   user_id = session[:id]
-  redirect("/") unless user_id
-  @permissions = db.execute("SELECT permissions from users WHERE id = ?", user_id).first
+  @permissions = DbAccesor.new.permissions(user_id)
+  
   if @permissions["permissions"] == 1
-    @characters = db.execute("SELECT * FROM characters")
+    @characters = DbAccesor.new.all_characters()
   else
-    @characters = db.execute("SELECT * FROM characters WHERE users_id = ?", user_id )
+    @characters = DbAccesor.new.specific_characters(user_id)
   end
-  p @characters
-  slim (:"characters/show")
+  slim (:"characters/index")
 end
 
-get '/adminSite' do
-  permissions = db.execute("SELECT permissions from users WHERE id = ?", session[:id]).first
-  if permissions["permissions"] == 1
-    @all_users = db.execute("SELECT * FROM users")
-  elsif session[:id] != nil
-    redirect("/index")
-  else
-    redirect("/")
-  end 
+# Display the site exclusive to the admin while rendering all users
+# 
+# @return [void]
+get '/adminSite/' do
+  @all_users = DbAccesor.new.all_users()
   slim :adminSite
 end
 
+# Delete the user with specified id from database
+# 
+# @option user_id [Integer] the id of the user to delete
+# @return [void]
 post "/adminSite/:id/delete" do
-  DbAccesor.new.delete_user(user_id = params[:id])
-  redirect('/adminSite')
+  user_id = params[:id]
+  DbAccesor.new.delete_user(user_id)
+  redirect('/adminSite/')
 end
 
+# Deletes a character from the database by ID. If the current user has admin permissions, the character will be deleted
+# regardless of who owns it. Otherwise, only the owner of the character can delete it.
+#
+# @option [Integer] :id The ID of the character to be deleted.
+# @return [void]
 post '/characters/:id/delete' do
-  permissions = db.execute("SELECT permissions from users WHERE id = ?", session[:id]).first
+  permissions = DbAccesor.new.permissions(session[:id])
   if permissions["permissions"] == 1
     DbAccesor.new.delete_character(id = params[:id].to_i)
   else
     charOwnerId = DbAccesor.new.userCheck(params[:id]).first
     redirect("/") unless charOwnerId[0] == session[:id] 
-    DbAccesor.new.delete_character(id = params[:id].to_i) 
+    DbAccesor.new.delete_character(params[:id].to_i) 
   end
-  redirect('/characters')
+  redirect('/characters/')
 end
  
+# Renders the character edit page for a specific character ID.
+#
+# @return [void] 
 get '/characters/:id/edit' do
-  permissions = db.execute("SELECT permissions from users WHERE id = ?", session[:id]).first
+  permissions = DbAccesor.new.permissions(session[:id])
   if permissions["permissions"] == 1
-    @username = db.execute('SELECT username FROM users WHERE id = ?', session[:id]).first
-    @character = db.execute("SELECT * FROM characters WHERE id = ?", params[:id]).first
+    @username = DbAccesor.new.username(session[:id])
+    @character = DbAccesor.new.specific_character(params[:id])
   else
     charOwnerId = DbAccesor.new.userCheck(params[:id]).first
     redirect("/") unless charOwnerId[0] == session[:id] 
-    @username = db.execute('SELECT username FROM users WHERE id = ?', session[:id]).first
-    @character = db.execute("SELECT * FROM characters WHERE id = ?", params[:id]).first
+    @username = DbAccesor.new.username(session[:id])
+    @character = DbAccesor.new.specific_character(params[:id])
   end
   slim (:"characters/edit")
 end
 
-post '/characters/:id/edit' do
-  permissions = db.execute("SELECT permissions from users WHERE id = ?", session[:id]).first
+# Edits an existing character from the database
+# 
+# @option [String] :name The name of the character
+# @option [String] :race The race of the character
+# @option [String] :class The characters class
+# @option [Integer] :level The characters level
+# @option [Integer] :str The strength of the character
+# @option [Integer] :dex The dexterity of the character
+# @option [Integer] :con The constitution of the character
+# @option [Integer] :int The intelligence of the character
+# @option [Integer] :wis The wisdom of the character
+# @option [Integer] :cha The charisma of the character
+# @option [Integer] :user_id The id of the user creating the character
+# 
+# @return [void]
+post '/characters/:id/update' do
+  permissions = DbAccesor.new.permissions(user_id)
   if permissions["permissions"] == 1  
     DbAccesor.new.edit_character( 
       name = params[:name],
@@ -181,30 +264,37 @@ post '/characters/:id/edit' do
         cha = params[:cha], 
         params[:id])
       end
-  redirect to('/characters')
+  redirect to('/characters/')
 end
 
-get '/characters/:id/play' do
-  # Get a specific character from the database using the id parameter
-  permissions = db.execute("SELECT permissions from users WHERE id = ?", session[:id]).first
+# Displays the details of a specific character with the given ID.
+# If the user has sufficient permissions, they can view any character.
+# Otherwise, they can only view characters that they own.
+# 
+# @option [Integer] :id the ID of the character to be displayed
+# @return [void]
+get '/characters/:id' do
+  permissions = DbAccesor.new.permissions(user_id)
   if permissions["permissions"] == 1
-    @username = db.execute('SELECT username FROM users WHERE id = ?', session[:id]).first
-    @character = db.execute("SELECT * FROM characters WHERE id = ?", params[:id]).first
+    @username = DbAccesor.new.username(session[:id])
+    @character = DbAccesor.new.specific_character(params[:id])
   else
     charOwnerId = DbAccesor.new.userCheck(params[:id]).first
     redirect("/") unless charOwnerId[0] == session[:id]
-    @username = db.execute('SELECT username FROM users WHERE id = ?', session[:id]).first
-    @character = db.execute("SELECT * FROM characters WHERE id = ?", params[:id]).first
+    @username = DbAccesor.new.username(session[:id])
+    @character = DbAccesor.new.specific_character(params[:id])
   end
-  slim (:"characters/play")
+  slim (:"characters/show")
 end
 
+# Display all items belonging to specific character
+# 
+# @return [void]
 get '/characters/:id/items' do 
-  permissions = db.execute("SELECT permissions from users WHERE id = ?", session[:id]).first
+  permissions = DbAccesor.new.permissions(user_id)
   if permissions["permissions"] == 1
     @character_id = params[:id].to_i
-    db.results_as_hash = true
-    @items = db.execute("SELECT * FROM items WHERE character_id = ?", @character_id) 
+    @items = DbAccesor.new.all_items(@character_id)
     if @items == nil
       @items = ['No items!']
     end
@@ -218,12 +308,15 @@ get '/characters/:id/items' do
       @items = ['No items!']
     end
   end
-  slim (:"items/items")
+  slim (:"items/index")
 end
 
-
+# Render the form to create a new item for a character.
+# @option [Integer] :id The id of the character to create a new item for
+# 
+# @return [void]
 get "/characters/:id/items/new" do
-  permissions = db.execute("SELECT permissions from users WHERE id = ?", session[:id]).first
+  permissions = DbAccesor.new.permissions(session[:id])
   if permissions["permissions"] == 1
     @result = params[:id]
   else
@@ -231,15 +324,25 @@ get "/characters/:id/items/new" do
     redirect("/") unless charOwnerId[0] == session[:id]
     @result = params[:id]
   end
-  slim (:"items/new_items")
+  slim (:"items/new")
 end
 
-
-post "/characters/:id/items/delete_items/:items_id" do
+# Delete item connected with specific character
+#
+# @option [Integer] :id the character's id
+# @option [Integer] :items_id the id of the item to delete
+post "/characters/:id/items/:items_id/delete" do
   DbAccesor.new.delete_items(@char_id = params[:id], @item = params[:items_id])
   redirect to("/characters/#{@char_id}/items")
 end
 
+# Create item for specific character
+# 
+# @option [Integer] :char_id The ID of the character to create the item for.
+# @option [String] :itemName The name of the item.
+# @option [String] :itemDescription The description of the item.
+
+# @return [void]
 post '/create_items/:char_id' do
   DbAccesor.new.create_items(
     itemName = params[:itemName],
@@ -249,13 +352,25 @@ post '/create_items/:char_id' do
   redirect to("/characters/#{@charIdForItems}/items")
 end
 
-get "/characters/:id/items/edit_items/:item_id" do
-  redirect("/") unless session[:id]
+# Display the edit item file
+#
+# @option [Integer] :id the character ID
+# @option [Integer] :item_id the item ID to edit
+
+# @return [void]
+get "/characters/:id/items/:item_id/edit" do
   @charID = params[:id]
   @ItemId = params[:item_id]
-  slim (:"items/edit_items")
+  slim (:"items/edit")
 end
 
+# Updates an item's name and description
+#
+# @option [String] :itemName The new name for the item
+# @option [String] :itemDescription The new description for the item
+# @option [Integer] :item_id The ID of the item to be updated
+#
+# @return [void]
 post "/edit_items/:item_id" do
   DbAccesor.new.edit_items(
     itemName = params[:itemName],
@@ -266,15 +381,63 @@ post "/edit_items/:item_id" do
   redirect to("/characters/#{charID}/items")
 end
 
+# Clears the session and renders the home page.
+# 
+# @return [void]
 get "/destroy" do
-  redirect("/") unless session[:id]
   session.clear
   slim :homePage
 end
 
-get '/shareChar' do
-  redirect("/") unless session[:id]
-  @all_characters = db.execute("SELECT * FROM characters")
-  @all_users = db.execute("SELECT * FROM users")
+# Displays all characters and users to be shared with
+#
+# @return [void]
+get '/shareChar/' do
+  @all_characters = DbAccesor.new.left_join_characters()
+  @all_users = DbAccesor.new.all_users()
+  
+  @relation = DbAccesor.new.order_likes_table(session[:id])
+   
+  @likes = []
+
+  for character in @all_characters
+      i = 0
+      for relation in @relation
+          if character["id"] == relation["char_id"]
+              @likes.append(true)
+              i += 1
+          end
+      end
+      if i == 0
+          @likes.append(false)
+      end
+  end 
   slim (:"characters/shareChar")
 end
+
+# Updates a like for shared character
+#
+# @option [Integer] :id the id of the shared character to be liked
+# @return [void]
+post '/shareChar/:id/update' do
+  charID = params[:id]
+  user_id = session[:id]
+
+  DbAccesor.new.update_likes(user_id,charID)
+
+  redirect to("/shareChar/")
+end
+
+# Deletes the likes of a shared character for the current user
+# 
+# @option [Integer] :id the ID of the character being shared
+# @return [void]
+post '/shareChar/:id/delete' do
+  user_id = session[:id]
+  charID = params[:id]
+  
+  DbAccesor.new.delete_likes(user_id, charID)
+
+  redirect to("/shareChar/")
+end
+
--- model.rb
+++ model.rb
@@ -3,53 +3,220 @@ require 'sinatra/reloader'
 require 'slim'
 require 'sqlite3'
 
+enable :sessions
 
+$db = SQLite3::Database.new("db/characters.db")
+$db.results_as_hash = true
 
+# class DbAccesor
+# 
+# This class contains all SQL code for interacting with the database file aswell as the Bcrypt code necessary for the login
 class DbAccesor
     
+    # Takes everything from the characters table
+    # 
+    # @return [Hash] containing all data
+    def all_characters()
+        $db.execute("SELECT * FROM characters")
+    end
+
+    # Takes all characters assigned to user_id
+    # 
+    # @param [Integer] user_id the user identification
+    # @return [Hash] all data from all characters with user_id
+    def specific_characters(user_id)
+        $db.execute("SELECT * FROM characters WHERE users_id = ?", user_id )
+    end
+
+    # Takes character which is attributed with specific char_id
+    # 
+    # @param [Integer] char_id thecharacter identification
+    # @return [Hash] all info from specific character
+    def specific_character(char_id)
+        $db.execute("SELECT * FROM characters WHERE id = ?", char_id).first
+    end
+    
+    # Create a new character connected to a used with used_id
+    # 
+    # @param [String] name the name of the character
+    # @param [String] race the race of the character
+    # @param [String] char_class the class of the character
+    # @param [Integer] level the characters level
+    # @param [Integer] str the strength score of the character
+    # @param [Integer] dex the dexterity score of the character
+    # @param [Integer] con the constitution score of the character
+    # @param [Integer] int the intelligence score of the character
+    # @param [Integer] wis the wisdom score of the character
+    # @param [Integer] cha the charisma score of the character
+    # @param [Integer] user_id the user_id assigned to the character
+    # @return [Hash] inserts character data into characters table 
     def create_character(name, race, char_class, level, str, dex, con, int, wis, cha, user_id)
-        db = SQLite3::Database.new("db/characters.db")
-        db.execute("INSERT INTO characters (name, race, class, level, str, dex, con, int, wis, cha, users_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", name, race, char_class, level, str, dex, con, int, wis, cha, user_id)
+        $db.execute("INSERT INTO characters (name, race, class, level, str, dex, con, int, wis, cha, users_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", name, race, char_class, level, str, dex, con, int, wis, cha, user_id)
     end
 
+    # Edit an existing character, chancing values
+    # 
+    # @param [String] name the name of the character
+    # @param [String] race the race of the character
+    # @param [String] char_class the class of the character
+    # @param [Integer] level the characters level
+    # @param [Integer] str the strength score of the character
+    # @param [Integer] dex the dexterity score of the character
+    # @param [Integer] con the constitution score of the character
+    # @param [Integer] int the intelligence score of the character
+    # @param [Integer] wis the wisdom score of the character
+    # @param [Integer] cha the charisma score of the character
+    # @param [Integer] id the characters identification
+    # @return [Hash] the updated values of the character
     def edit_character(name, race, char_class, level, str, dex, con, int, wis, cha, id)
-        db = SQLite3::Database.new("db/characters.db")
-        db.execute("UPDATE characters SET name = ?, race = ?, class = ?, level = ?, str = ?, dex = ?, con = ?, int = ?, wis = ?, cha = ? WHERE id = ?", name, race, char_class, level, str, dex, con, int, wis, cha, id)
+        $db.execute("UPDATE characters SET name = ?, race = ?, class = ?, level = ?, str = ?, dex = ?, con = ?, int = ?, wis = ?, cha = ? WHERE id = ?", name, race, char_class, level, str, dex, con, int, wis, cha, id)
     end
 
+
+    # Takes all items which are assigned to a specific character id
+    # 
+    # @param [Integer] char_id the character identification 
+    # @return [Hash] returns everything from items table which is assigned to char_id
+    def all_items(char_id)
+        $db.execute("SELECT * FROM items WHERE character_id = ?", char_id) 
+    end
+
+    # Create new item connected with a character with specific char_id
+    # 
+    # @param [String] itemName the name of the item
+    # @param [String] itemDescription the description of the item
+    # @param [Integer] character_id the id to a specific character
+    # @return [Hash] inserts data from params into the table items
     def create_items(itemName, itemDescription, character_id) 
-        db = SQLite3::Database.new("db/characters.db")
-        db.execute("INSERT INTO items (name, description, character_id) VALUES (?, ?, ?)", itemName, itemDescription, character_id)
+        $db.execute("INSERT INTO items (name, description, character_id) VALUES (?, ?, ?)", itemName, itemDescription, character_id)
     end
 
+    # Update item connected with specific id
+    # 
+    # @param [String] itemName the name of the item
+    # @param [String] itemDescription the description of the item
+    # @param [Integer] id the id to a specific item
+    # @return [Hash] updates the values in the table with new values
     def edit_items(itemName, itemDescription, id)
-        db = SQLite3::Database.new("db/characters.db")
-        db.execute("UPDATE items SET name = ?, description = ? WHERE id = ?",itemName, itemDescription, id)
+        $db.execute("UPDATE items SET name = ?, description = ? WHERE id = ?",itemName, itemDescription, id)
     end
 
+    # Delete character with specific character-id
+    # 
+    # @param [Integer] id the id of specific character
+    # @return [Hash] delete the character and the items connected to it
     def delete_character(id) 
-        db = SQLite3::Database.new("db/characters.db")
-        db.execute("DELETE FROM characters WHERE id = ?",id)
-        db.execute("DELETE FROM items WHERE character_id = ?", id)
+        $db.execute("DELETE FROM characters WHERE id = ?",id)
+        $db.execute("DELETE FROM items WHERE character_id = ?", id)
     end
-
+    
+    # Delete specific item from specific character
+    # 
+    # @param [Integer] id the id of the character
+    # @param [Integer] items_id  the id of the item
+    # @return [Hash] delete everything from items that correspond to the correct item_id and character id
     def delete_items(id, items_id)
-        db = SQLite3::Database.new("db/characters.db")
-        db.execute("SELECT * FROM items WHERE character_id = ?", id)
-        db.execute("DELETE FROM items WHERE id =?", items_id)
+        $db.execute("SELECT * FROM items WHERE character_id = ?", id)
+        $db.execute("DELETE FROM items WHERE id =?", items_id)
     end
 
+    # Delete the user and everything that is connected to the user
+    # 
+    # @param [Integer] user_id the id of the user
+    # @return [Hash] delete the user aswell as the characters and items connected with the user
     def delete_user(user_id)
-        db = SQLite3::Database.new("db/characters.db")
-        db.execute("DELETE FROM users WHERE id=?", user_id)
-        db.execute("DELETE FROM characters WHERE id = ?", user_id)
-        db.execute("DELETE FROM items WHERE id = ?", user_id)
+        $db.execute("DELETE FROM users WHERE id=?", user_id)
+        $db.execute("DELETE FROM characters WHERE id = ?", user_id)
+        $db.execute("DELETE FROM items WHERE id = ?", user_id)
     end
 
+    # Select a specific username
+    # 
+    # @param [Integer] user_id the id of the user
+    # @return [Hash] returns the username connected with the user_id
+    def username(user_id)
+        $db.execute('SELECT username FROM users WHERE id = ?', user_id).first
+    end
+
+    # Select all users from the database users
+    # 
+    # @return [Hash] returns all the data in the table
+    def all_users()
+        $db.execute("SELECT * FROM users")
+    end
+
+    # Selects everything from user with specific username
+    # 
+    # @param [String] username the username of the user
+    # @return [Hash] returns everything from the table with corresponding username
+    def specific_user(username)
+        return $db.execute('SELECT * FROM users WHERE username = ?', username).first
+    end
+
+    # A check used to verify if the character belongs to a user
+    # 
+    # @param [Integer] char_id the id of the character
+    # @return [Hash] returns the user_id from the characters table which is connected with the character id
     def userCheck(char_id)
-        db = SQLite3::Database.new("db/characters.db")
-        return db.execute("SELECT users_id FROM characters WHERE id = ?", char_id)
+        return $db.execute("SELECT users_id FROM characters WHERE id = ?", char_id)
     end
 
+    # Check if the password of the user put in matches with the database
+    # 
+    # @param [String] password the password put in by the user
+    # @param [String] check_password the password going through the Bcrypt class
+    # @param [Integer] user_id the id used to get the correct username
+    # @return [String, Integer, nil, nil] returns the username and the user_id, if there is nothing there it returns nil
+    def passwordCheck(password, check_password, user_id)
+        if (BCrypt::Password.new(check_password) == password)
+            username = DbAccesor.new.username(user_id)
+            return username, user_id
+          else
+            return nil, nil
+          end
+    end
+
+    # Checks if user has the value needed in permissions
+    # 
+    # @param [Integer] user_id the if of the user
+    # @return [Hash] returns the key value pair: "permissions"
+    def permissions(user_id)
+        $db.execute("SELECT permissions from users WHERE id = ?", user_id).first
+    end
+
+    # Orders the likes table to ascending
+    # 
+    # @param [Integer] user_id the id of the user
+    # @return [Hash] selects everything from the table likes depending on user_id
+    def order_likes_table(user_id)
+        $db.execute("SELECT * FROM likes WHERE user_id = ? ORDER BY char_id ASC", user_id)
+    end
+
+    # Connects the needed columns into a new table for the likes system to work
+    # 
+    # @return [Hash] returns a new table through JOIN with the neccesary columns in place
+    def left_join_characters()
+        $db.execute("WITH tmp(char_id, likes) AS (SELECT char_id,COUNT(*) FROM likes GROUP BY char_id)
+            SELECT id,name,class,race,level,str,dex,con,int,wis,cha,users_id, COALESCE(tmp.likes,0) AS likes FROM characters
+            LEFT JOIN tmp ON tmp.char_id = characters.id;") 
+    end
+
+    # Inserts into table likes the user id and character id
+    # 
+    # @param [Integer] user_id the id of the user
+    # @param [Integer] charID the id of the character
+    # @return [Hash] inserts the ids into the table
+    def update_likes(user_id, charID)
+        return $db.execute("INSERT INTO likes (user_id, char_id) VALUES (?, ?)",user_id, charID)
+    end
+
+    # Takes away a like from the table likes
+    # 
+    # @param [Integer] user_id the if of the user
+    # @param [Integer] charID the id of the character
+    # @return [Hash] deletes from the like table depending on the ids
+    def delete_likes(user_id, charID)
+        return $db.execute("DELETE FROM likes WHERE (user_id = ?) AND (char_id = ?)", user_id, charID)
+    end
 end
 
